6af0df52e53b38cd216f25f69992340aadf003c7
Andréas CASTELLO <andreas.castello06@gmail.com>
2022-02-19 15:13:53 +0100

    syntaxe_moderne

---
+++ syntaxe_moderne/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include "swap.h"
+
+int main(){
+    auto i { 1 };
+    auto j = 2;
+    swap(i, j);
+
+    return EXIT_SUCCESS;
+}
---
+++ syntaxe_moderne/swap.cpp
@@ -0,0 +1,6 @@
+#include "swap.h"
+auto swap (int a, int b) -> void {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
