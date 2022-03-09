8300103a07c3fbd7ba39cb8bf7d2f483a95ce6fc
Andréas CASTELLO <andreas.castello06@gmail.com>
2022-03-09 18:50:52 +0100

    destructeur

---
+++ destructeur/voiture.hpp
@@ -0,0 +1,23 @@
+#include <string>
+
+class voiture
+{
+private:
+    /* data */
+public:
+    voiture(/* args */);
+
+    //destructeur
+    ~voiture();
+};
+
+
+
+// les lignes en dessous devraient se situer dans un voiture.cpp
+voiture::voiture(/* args */)
+{
+}
+
+voiture::~voiture()
+{
+}
