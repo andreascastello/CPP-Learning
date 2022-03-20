3da1fe145baf2253409ebb90e67dd2d9236d606b
Andréas CASTELLO <andreas.castello06@gmail.com>
2022-03-20 12:55:23 +0100

    template paramètre et valeur par default

---
+++ param-template/Chapitre6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+template< typename Type=int, typename Conteneur = std::vector<Type>,typename Index=int, Index taille = 100 /* valeur par default */>
+class Pile {
+	Conteneur data;
+	Index index;
+
+public:
+	Pile(/* valeur par default (taille)*/) : data(taille), index(0) {}
+
+	void empile(Type valeur) {
+		std::cout << " empile : " << valeur << std::endl;
+		data.push_back(valeur);
+		index++;
+	}
+};
+
+auto main() -> int {
+	
+	Pile<> pi;
+	pi.empile(42);
+
+
+	return 0;
+}
